0d81a225def146f107ca9f4cb50df94da594862b
undefined-pixel <mariankowall@gmail.com>
2026-04-03 09:33:20 +0200

    fix scripts

--- work_scripts/copy_config_from_host_to_repo.sh
+++ work_scripts/copy_config_from_host_to_repo.sh
@@ -12,9 +12,9 @@ set -xe
 rsync -av $HOME/.config/zed/settings.json $HOME/repo/Shell_dot_file/mac/.config/zed/settings.json
 rsync -av $HOME/.config/zed/keymap.json $HOME/repo/Shell_dot_file/mac/.config/zed/keymap.json
 rsync -av $HOME/.config/.myshellconfig.sh $HOME/repo/Shell_dot_file/linux/.config/.myshellconfig.sh
-rsync -av $HOME/.config/fastfetch $HOME/repo/Shell_dot_file/linux/.config/fastfetch/
-rsync -av $HOME/.config/nvim $HOME/repo/Shell_dot_file/linux/.config/nvim/
-rsync -av $HOME/.config/kitty $HOME/repo/Shell_dot_file/linux/.config/kitty/
+rsync -av $HOME/.config/fastfetch/ $HOME/repo/Shell_dot_file/linux/.config/fastfetch/
+rsync -av $HOME/.config/nvim/ $HOME/repo/Shell_dot_file/linux/.config/nvim/
+rsync -av $HOME/.config/kitty/ $HOME/repo/Shell_dot_file/linux/.config/kitty/
 rsync -av $HOME/.config/wezterm/ $HOME/repo/Shell_dot_file/linux/.config/wezterm/
 rsync -av $HOME/.config/i3/ $HOME/repo/Shell_dot_file/linux/.config/i3/
 rsync -av $HOME/.config/tmux/ $HOME/repo/Shell_dot_file/linux/.config/tmux/
@@ -23,6 +23,6 @@ rsync -av $HOME/.config/tmux/ $HOME/repo/Shell_dot_file/linux/.config/tmux/
 
 OS=$(uname)
 if [[ "$OS" == "Darwin" ]]; then
-  rsync -av $HOME/.config/sketchybar $HOME/repo/Shell_dot_file/mac/.config/sketchybar/
+  rsync -av $HOME/.config/sketchybar/ $HOME/repo/Shell_dot_file/mac/.config/sketchybar/
   rsync -av $HOME/.config/borders/ $HOME/repo/Shell_dot_file/mac/.config/borders/
 fi
--- work_scripts/copy_only_mac_things.sh
+++ work_scripts/copy_only_mac_things.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -xe
-rsync -av $HOME/.config/sketchybar $HOME/repo/Shell_dot_file/mac/.config/sketchybar/
+rsync -av $HOME/.config/sketchybar/ $HOME/repo/Shell_dot_file/mac/.config/sketchybar/
 rsync -av $HOME/.config/borders/ $HOME/repo/Shell_dot_file/mac/.config/borders/

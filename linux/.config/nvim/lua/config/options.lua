d8fff08f60203b1bdcd5e5bc605235112092b765
undefined-pixel <mariankowall@gmail.com>
2026-04-07 12:57:30 +0200

    add new files

---
+++ linux/.config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+-- Use system clipboard so yank works in browser
+vim.opt.clipboard = "unnamedplus"
